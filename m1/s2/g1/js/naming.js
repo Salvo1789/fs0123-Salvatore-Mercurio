0d0e8f20f7d8b0cc3d445ee6bbd0f34b568fd146
Salvo1789 <salvo.mercurio89@gmail.com>
2023-02-06 15:25:53 +0100

    Week 2 Day 1

---
+++ m1/s2/g1/js/naming.js
@@ -0,0 +1,18 @@
+var pet_preferito = 'gatto';
+console.log(pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+console.log(petPreferito);
+var PetPreferito = 'coniglio';
+console.log(PetPreferito);
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+//var 12 = 'numero';
