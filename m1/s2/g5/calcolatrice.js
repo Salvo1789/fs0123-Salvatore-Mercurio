a9f1db344cdd4908871a09ea70bef616d82e1564
Salvo1789 <salvo.mercurio89@gmail.com>
2023-02-10 17:32:39 +0100

    Week 2 Day 5 - fixed

---
+++ m1/s2/g5/calcolatrice.js
@@ -0,0 +1,17 @@
+function aggiungiSimbolo(elemento){
+
+    let simbolo = elemento.getAttribute('data-simbolo');
+    let display = document.querySelector('#display');
+    display.value += simbolo;
+}
+
+function totale(){
+    let display = document.querySelector('#display');
+    display.value = eval(display.value);
+}
+
+function cancella(elemento){
+    let simbolo = elemento.getAttribute('data-simbolo');
+    let display = document.querySelector('#display');
+    display.value = simbolo;
+}
